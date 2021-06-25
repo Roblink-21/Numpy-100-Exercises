dae0ae43b4f61f44dd831c9da1b3d05751a64834
luckLuis <58127103+luckLuis@users.noreply.github.com>
2021-06-24 22:08:56 -0500

    Add files via upload

---
+++ exercisePython#81.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+def cartesian(arrays):
+    arrays = [np.asarray(a) for a in arrays]
+    shape = (len(x) for x in arrays)
+
+    ix = np.indices(shape, dtype=int)
+    ix = ix.reshape(len(arrays), -1).T
+
+    for n, arr in enumerate(arrays):
+        ix[:, n] = arrays[n][ix[:, n]]
+
+    return ix
+
+print (cartesian(([1, 2, 3], [4, 5], [6, 7])))
+
+
+# In[ ]:
+
+
+
+
